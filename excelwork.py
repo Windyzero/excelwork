464481283f9f39d1864c37e25febb29703a8cc79
Windyzero <txd521521@126.com>
2018-10-30 18:04:57 +0800

    commit

--- excelwork.py
+++ excelwork.py
@@ -8,27 +8,49 @@ sheets = originData.sheets()
 firstSheet = sheets[0]
 secondSheet = sheets[1]
 
+titles = firstSheet.row_values(1)
 firstStudents = []
 for i in range (2, firstSheet.nrows):
 	firstStudents.append(firstSheet.row_values(i))
 
-firstStudents.sort(key = lambda student : student[3], reverse = True)
+def sortSubject(col):
+	title = titles[col]
+	titles.insert(col+1, title + "rank")
+	firstStudents.sort(key = lambda student : student[col], reverse = True)
+	preScore = 0;
+	preRank = 0;
+	for i in range (len(firstStudents)):
+		if firstStudents[i][col] == preScore:
+			firstStudents[i].insert(col+1, preRank)
+		else:
+			preRank = i+1
+			firstStudents[i].insert(col+1, i+1)
+		preScore = firstStudents[i][col];
+	return True
+	
+sortSubject(3)
+sortSubject(5)
+sortSubject(7)
+sortSubject(9)
+sortSubject(11)
+sortSubject(13)
+firstStudents.sort(key = lambda student : student[1])
 
 output = xlwt.Workbook(encoding = 'utf-8')
 outSheet = output.add_sheet('result')
-length = len(firstSheet.row_values(1))
-for i in range (length):
-	outSheet.write(0, i, firstSheet.row_values(1)[i])
 
-outSheet.write(0, 9, "总分")
+for i in range (len(titles)):
+	outSheet.write(0, i, titles[i])
+
+#outSheet.write(0, 10, "总分")
 
 for i in range (len(firstStudents)):
 	for j in range (len(firstStudents[i])):
 		outSheet.write(i+1, j, firstStudents[i][j])
-	sum = 0
-	for j in range (3, 9):
-		sum = sum + firstStudents[i][j]
-	outSheet.write(i+1, 9, sum)
+#	sum = 0
+#	for j in range (3, 9):
+#		sum = sum + firstStudents[i][j]
+#	outSheet.write(i+1, 9, sum)
 		
 output.save('result.xls')
 print "done"
